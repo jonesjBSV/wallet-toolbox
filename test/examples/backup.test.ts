3c333e37974e264ae887332366a096f1e477aeeb
Tone Engel <tone@kizmet.org>
2025-02-21 08:51:11 -0500

    test backup of bob and bob2

---
+++ test/examples/backup.test.ts
@@ -0,0 +1,55 @@
+import { Setup, SetupEnv, SetupWallet } from "../../src"
+import dotenv from "dotenv"
+dotenv.config()
+
+describe('backup example tests', () => {
+    jest.setTimeout(99999999)
+
+    if (Setup.noEnv('test')) return
+    const env = Setup.getEnv('test')
+
+    test('1 backup bob', async () => {
+      await backupWalletClient(env, process.env.BOB_TEST_IDENTITY || '')
+    })
+
+    test('2 backup bob2', async () => {
+      await backupWalletClient(env, process.env.BOB2_TEST_IDENTITY || '')
+    })
+})
+
+/**
+ * @publicbody
+ */
+export async function backup(): Promise<void> {
+  const env = Setup.getEnv('test')
+  await backupWalletClient(env, env.identityKey)
+}
+
+/**
+ * @publicbody
+ */
+export async function backupWalletClient(env: SetupEnv, identityKey: string): Promise<void> {
+  const setup = await Setup.createWalletClient({ env, rootKeyHex: env.devKeys[identityKey] })
+  await backupToSQLite(setup)
+  await setup.wallet.destroy()
+}
+
+/**
+ * @publicbody
+ */
+export async function backupToSQLite(setup: SetupWallet, filePath?: string, databaseName?: string): Promise<void> {
+  const env = Setup.getEnv(setup.chain)
+  filePath ||= `backup_${setup.identityKey}.sqlite`
+  databaseName ||= `${setup.identityKey} backup`
+  
+  const { activeStorage: backup, storage, wallet } = await Setup.createWalletSQLite({
+    env,
+    filePath,
+    databaseName,
+    rootKeyHex: setup.keyDeriver.rootKey.toHex()
+  })
+
+  await setup.storage.addWalletStorageProvider(backup)
+
+  await setup.storage.updateBackups()
+}
